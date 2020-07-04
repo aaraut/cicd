440c8094542ef770fe4a0f6f33c0c20d6940f9a4
alekhraut <alekh.raut3@gmail.com>
2020-07-04 16:10:29 +0530

    Commented code

--- src/app/survey-form/survey-form.component.ts
+++ src/app/survey-form/survey-form.component.ts
@@ -10,34 +10,36 @@ import { ApiService } from '../api.service';
 export class SurveyFormComponent implements OnInit{
 
 constructor(private apiService: ApiService){}
-
-optionsRecommend = ['Likely', 'Very Likely', 'Not Likely']; 
-
-list: any[];
-
-survey = new StudentSurvey();
-sample: any;
-test: any;
-submitted = false;
-
 ngOnInit() {
-}
+  }
 
-onSubmit(){ 
-  this.sample = JSON.stringify(this.survey);
+// optionsRecommend = ['Likely', 'Very Likely', 'Not Likely']; 
 
-  console.log(this.survey);
-  this.test={"body": this.sample};
+// list: any[];
+
+// survey = new StudentSurvey();
+// sample: any;
+// test: any;
+// submitted = false;
+
+// ngOnInit() {
+// }
+
+// onSubmit(){ 
+//   this.sample = JSON.stringify(this.survey);
+
+//   console.log(this.survey);
+//   this.test={"body": this.sample};
   
- //this.test={"body": "{\"fname\":\"Devika4\",\"lname\":\"W\",\"add\":\"4307 Ramona Drive\",\"email\":\"s@gmail.com\",\"zip\":88888,\"sdate\":\"2019-12-07\", \"city\":\"Herndon\", \"state\":\"Chicago\", \"phone\":999}"}
-  this.apiService.createSurvey(this.test).subscribe((res)=>{
-    console.log("Student Survey Saved!");
-  });
-  this.submitted = true; 
-  console.log("Print data",this.survey);
-}
+//  //this.test={"body": "{\"fname\":\"Devika4\",\"lname\":\"W\",\"add\":\"4307 Ramona Drive\",\"email\":\"s@gmail.com\",\"zip\":88888,\"sdate\":\"2019-12-07\", \"city\":\"Herndon\", \"state\":\"Chicago\", \"phone\":999}"}
+//   this.apiService.createSurvey(this.test).subscribe((res)=>{
+//     console.log("Student Survey Saved!");
+//   });
+//   this.submitted = true; 
+//   console.log("Print data",this.survey);
+// }
 
-// TODO: Remove this when we're done
-get diagnostic() { return JSON.stringify(this.survey); }
+// // TODO: Remove this when we're done
+// get diagnostic() { return JSON.stringify(this.survey); }
 
 }
